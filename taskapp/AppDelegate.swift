1a1307123b480b16d745c589a6834c1bdca8fc97
Kentaro.Nishikata <kentaro.nishikata@gmail.com>
2021-01-21 19:33:54 +0900

    オリジナルの状態

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -7,16 +7,45 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
 
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    // 色
+    let tealblueColor = UIColor(red: 90.0/255.0, green: 200.0/255.0, blue: 250.0/255.0, alpha:1)
+    let whiteColor = UIColor(red: 236.0/255.0, green: 254.0/255.0, blue: 234.0/255.0, alpha:1)
+    let gray6Color = UIColor(red: 28/255.0, green: 28.0/255.0, blue: 30.0/255.0, alpha:1)
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        // ユーザに通知の許可を求める --- ここから ---
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+            // Enable or disable features based on authorization
+        } // --- ここまで追加 ---
+        center.delegate = self     // 追加
+        
+        
+        // ① ナビゲーションバーの背景色
+        UINavigationBar.appearance().barTintColor = tealblueColor
+
+        // ② ナビゲーションバーのタイトルの色
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: whiteColor]
+
+        // ③ ナビゲーションバー上のアイテムの色
+        UINavigationBar.appearance().tintColor = gray6Color
+        
         return true
+        
     }
+    
+    // アプリがフォアグラウンドの時に通知を受け取ると呼ばれるメソッド --- ここから ---
+        func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+            completionHandler([.alert, .sound])
+        } // --- ここまで追加 ---
 
     // MARK: UISceneSession Lifecycle
 
